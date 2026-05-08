f4b6bcf2242bb7db997d727ecc6cf83a6c62d80d
Ibrahim Ghanem <ibrahimghanem052@gmail.com>
2026-05-08 05:27:35 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php 
 
-    $conn = mysqli_connect("localhost", "root", "", "city_events");
+    $conn = mysqli_connect("sql311.infinityfree.com", "if0_41781840", "svueventshub", "if0_41781840_city_events","3306");
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
